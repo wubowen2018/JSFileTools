7c88bb60c47c0e2d65f8197dd23b7d5f13ec5359
wubowen2018 <wubowen2017@gmail.com>
2020-07-11 19:13:18 +0800

    增加复制功能

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var mkdir = require('./lib/mkdir')
 var rmdir = require('./lib/rmdir')
 var touch = require('./lib/touch')
 var remove = require('./lib/remove')
+var copy = require('./lib/copy')
+var cut = require('./lib/cut')
 
 module.exports={
     mkdir,
     rmdir,
     touch,
-    remove
+    remove,
+    copy,
+    cut
 }
--- test/index.js
+++ test/index.js
@@ -1,16 +1,24 @@
-const {mkdir,rmdir,touch,remove} = require("../index")
+const {mkdir,rmdir,touch,remove,copy,cut} = require("../index")
+var fs = require('fs')
 
-touch("./test/temp/aa/bb.txt")
-touch("./test/temp/aa/bb")
-mkdir("./test/temp/cc")
-remove("./test/temp/aa/bb")
+// touch("./test/temp/aa/bb.txt")
+// touch("./test/temp/aa/bb")
+// mkdir("./test/temp/cc")
+// remove("./bb.txt")
 
 // rmdir("./test/temp")
+// copy("./test/temp/", ".")
+// remove("./test/temp/aa/bb.txt")
+// rmdir("./test/temp")
+
+
 
 
 console.log('===============================');
 
-
+// var str = "./test/temp/aa/bb.txt"
+// var aa = str.match(/\/([^\/]+)$/g)[0]
+// console.log(aa);
 
 
 
