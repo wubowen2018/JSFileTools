88cb55e2a21d91d7cc29495b9632e407619172cb
wubowen2018 <wubowen2017@gmail.com>
2020-07-11 12:09:26 +0800

    try to push to https://registry.npm.taobao.com

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-console.log("hello world!");
+console.log("=====================================");
+let str = "./hello/test"
+var pathArr = str.split('/')
+// console.log(pathArr);
+
+var path1 = pathArr.slice(0,1)
+var path12 = pathArr.slice(0,1).join('/')
+var path2 = pathArr.slice(0,2)
+var path3 = pathArr.slice(0,3)
+
+console.log(path3,path12);
+
+
+
+
+console.log("=====================================");
 
 
