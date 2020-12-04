9c3a7e253a1625cb7f942b93bd9bf8b01790e0c9
wubowen2018 <wubowen2017@gmail.com>
2020-12-05 01:03:50 +0800

    三门问题模拟

--- test/index.js
+++ test/index.js
@@ -15,32 +15,62 @@ var fs = require('fs')
 // rmdir("./test/temp")
 // cut('./test/temp','.')
 
-
-
-
-
 console.log('===============================');
-
-
-
 // var str = "aaaabbbbbcc"
 // var reg = /(\w)\1*/g
 // var temp = str.replace(reg,"$1")
 // console.log(temp);  
 
-let mSet = new Set(["jimmy","Jeremy","jim"])
-mSet.add("wubowen");
-console.log(mSet);
+// let mSet = new Set(["jimmy","Jeremy","jim"])
+// mSet.add("wubowen");
+// console.log(mSet);
 
-mSet.delete("jim");
-console.log(mSet);
-console.log(mSet.size);
+// mSet.delete("jim");
+// console.log(mSet);
+// console.log(mSet.size);
 
-let str = "hello wobowen"
-for (let index = 0; index < str.length; index++) {
-    !(str[index] == " ") && mSet.add(str[index])  
+//利用set放入不重复给数组去重
+// let str = "hello wobowen"
+// for (let index = 0; index < str.length; index++) {
+//     !(str[index] == " ") && mSet.add(str[index])  
+// }
+// console.log(mSet);
+let getCar = 0
+
+let misCar = 0
+
+function setPrize(){
+    let prizeArr = ["山羊","山羊","汽车"];
+    prizeArr.sort( () => Math.random() - 0.5 )
+    return prizeArr;
 }
-console.log(mSet);
+
+
+for (let i = 0; i < 1000; i++) {
+    const arr = setPrize()
+    const index = parseInt(Math.random() * 3)
+    arr.splice(index, 1);
+
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] === "山羊" ) {
+            arr.splice(j, 1); 
+        }
+    }
+
+    if(arr[0] === "汽车"){
+        getCar++
+    } else{
+        misCar++ 
+    }  
+}
+    
+console.log(getCar, misCar );
+
+
+
+
+
+
 
 
 
