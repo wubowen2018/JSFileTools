f5db170447ee9aa2395059b4507a95e2a54257ab
wubowen2018 <wubowen2017@gmail.com>
2020-12-11 16:25:40 +0800

    安装cors 两种方法 实现跨域  拆分lib

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-var mkdir = require('./lib/mkdir')
-var rmdir = require('./lib/rmdir')
-var touch = require('./lib/touch')
-var remove = require('./lib/remove')
-var copy = require('./lib/copy')
-var cut = require('./lib/cut')
-//require('./module/*.js')
+let { logWarning, logTime, log } = require('./util')
+let express = require('express')
 
-module.exports={
-    mkdir,
-    rmdir,
-    touch,
-    remove,
-    copy,
-    cut
+//cors 跨域
+let cors = require('cors')
+
+
+let app = express()
+
+
+//cors的配置项
+var corsOptions = {
+    //origin: 'http://www.baidu.com', //只有百度可以访问
+    origin: '*',
+    optionsSuccessStatus: 200
 }
+
+//利用中间件实现跨域
+app.use('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+    //res.header('Access-Control-Allow-Origin', 'http://www.baidu.com'); //这样写，只有www.baidu.com 可以访问。
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+    if (req.method == 'OPTIONS') {
+        res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+    }
+    else {
+        next();
+    }
+});
+
+app.get('/info', function (req, res, next) {
+    res.json({ msg: 'get data successfully!' })
+})
+
+
+app.listen(3000, () => log("服务启动，监听端口3000..."))
+
+
+
+
--- test/index.js
+++ test/index.js
@@ -1,47 +1,9 @@
 const chalk = require('chalk')
 var { adjustPath, logWarning, logTime, log} = require('../util')
-const {mkdir,rmdir,touch,remove,copy,cut} = require("../index")
 var fs = require('fs')
-
-// touch("./test/temp/aa/bb.txt")
-// touch("./test/temp/aa/bb")
-// mkdir("./test/temp/cc")
-// remove("./bb.txt")
-
-// rmdir("./cc")
-
-// copy("./test/temp/", ".")
-// remove("./test/temp/aa/bb.txt")
-// rmdir("./test/temp")
-// cut('./test/temp','.')
 
 console.log('===============================');
 logTime(new Date().toString().match(/(\d\d:){2}\d{2}/g))
-// var str = "aaaabbbbbcc"
-// var reg = /(\w)\1*/g
-// var temp = str.replace(reg,"$1")
-// console.log(temp);  
-
-// let mSet = new Set(["jimmy","Jeremy","jim"])
-// mSet.add("wubowen");
-// console.log(mSet);
-
-// mSet.delete("jim");
-// console.log(mSet);
-// console.log(mSet.size);
-
-//利用set放入不重复给数组去重
-// let str = "hello wobowen"
-// for (let index = 0; index < str.length; index++) {
-//     !(str[index] == " ") && mSet.add(str[index])  
-// }
-// console.log(mSet);
-
-
-
-
-
-
 
 
 
