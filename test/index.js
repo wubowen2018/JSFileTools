c2aadac525daa0698fed1e1339c0f8e858bd2796
wubowen2018 <wubowen2017@gmail.com>
2020-12-21 18:18:43 +0800

    测试 对象数组依据某个属性排序

--- test/index.js
+++ test/index.js
@@ -1,14 +1,37 @@
 var { adjustPath, logWarning, logTime, log} = require('../util')
 
 console.log('===============================');
-// logTime(new Date().toString().match(/(\d\d:){2}\d{2}/g))
-logTime(new Date().toString())
+logTime(new Date().toString().match(/(\d\d:){2}\d{2}/g))
 
 
-let arr = [1,25,6,98,42,30]
-let newArr = arr.sort((x,y)=>{return x-y})
-log(arr)
-log(newArr)
+let arr = [
+    {
+        id:0,
+        name:"a",
+        money: '12'
+    },
+    {
+        id: 1,
+        name: "b",
+        money: '244.6'
+    },
+    {
+        id: 2,
+        name: "c",
+        money: '245.8'
+    },
+    {
+        id: 3,
+        name: "d",
+        money: '36.05'
+    }
+]
+arr.sort((x, y) => { return x.money - y.money})
+
+for (const mem of arr) {
+    console.log(`{id:${mem.id},name:${mem.name},money:${mem.money}}`);
+}
+
 
 
 
